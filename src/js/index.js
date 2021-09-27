1bbdd4b5c35b58cee9edc410aca3027b1ebc37e1
Konrad Bujas <kondziobu@gmail.com>
2021-09-27 15:48:39 +0200

    add js actions on buttons & cache and icons

--- src/js/index.js
+++ src/js/index.js
@@ -13,20 +13,33 @@ const buttonAdd = document.querySelector('.button--js');
 const buttonRemove = document.querySelector('.button-remove--js');
 const key = new Date().toISOString().slice(0, 10);
 
+let currentGlassCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+    currentGlassCounter = localStorageValue;
+}   else {
+    localStorage.setItem(key, 0);
+}
+
+glassCounter.innerHTML = currentGlassCounter;
+
 console.log(key);
 
+glassCounter.innerHTML = currentGlassCounter;
 
-
-let currentGlassCounter = 0;
 
 buttonAdd.addEventListener('click', () => {
     currentGlassCounter++;
     glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
 });
 
 buttonRemove.addEventListener('click', () => {
     if (currentGlassCounter > 0) {
     currentGlassCounter--;
     glassCounter.innerHTML = currentGlassCounter;
+    localStorage.setItem(key, currentGlassCounter);
     }    
 });
